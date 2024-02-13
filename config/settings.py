c58b3798a38efc98f06aae2a513a6c8b305e7d6b
Abdukarim K <abdukarimkhusenov@gmail.com>
2024-02-13 09:28:48 -0500

    heroku uchun

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bgf=rc8b@vz%63j$hasa3x_o_cke9qb30zakln(w9orfx!&44e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
